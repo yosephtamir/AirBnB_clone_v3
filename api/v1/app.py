713554668bac7e3754011e9475abeacbe8e70b46
Yoseph Tamirat <yosephtamir@gmail.com>
2024-01-06 20:57:58 +0300

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is used to run the flask web app"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
@@ -17,6 +17,12 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 def teardown_db(error):
     """used to close the database after using it"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Make a response if there is no webpage found"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
